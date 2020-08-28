4d1ba134e027f3041e21f13ae42095a67c70869e
wangting <2474870811@qq.com>
2020-08-28 10:16:58 +0800

    基础basic提交

--- app/src/main/java/com/example/android/youhua/youhua_oom/ToastUtil.java
+++ app/src/main/java/com/example/android/youhua/youhua_oom/ToastUtil.java
@@ -8,7 +8,10 @@ import android.widget.Toast;
  */
 public class ToastUtil {
     private static Toast mToast = null;
-
+    //长生命周期对象持有短生命周期对象，导致短生命周期对象不能回收。
+    //首先单例模式当中SingleInstance对应的实例是静态的，静态变量被创建之后，是被所有的该类对象公用的，
+    //静态变量的生命周期是跟随类的生命周期的，也就是只有该类被jvm从内存中干掉后，这个对应的实例的生
+    //命周期也就结束了，占用的内存也就被回收了。
     /**
      * 这里如果传入context会报内存泄漏；应该传入ApplicationContext
      * @param context
--- app/src/main/java/com/example/lifecycle/MyLifeCycleObserver.java
+++ app/src/main/java/com/example/lifecycle/MyLifeCycleObserver.java
@@ -11,7 +11,7 @@ public class MyLifeCycleObserver implements LifecycleObserver {
     private String TAG = MyLifeCycleObserver.class.getSimpleName();
     public MyLifeCycleObserver(){
         //这里MyLifeCycleObserver对LifeCycleOwner的ON_RESUME和ON_PAUSE方法进行了观察，
-       // 在LifeCycleOwner的生命周期产生变化的时候会调用LifeCycleObserver中注解修饰的方法。
+        //在LifeCycleOwner的生命周期产生变化的时候会调用LifeCycleObserver中注解修饰的方法。
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
